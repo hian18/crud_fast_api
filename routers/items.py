d9fa37cf00ab78b9e82d9e259c2c1b5a95a25247
hian aguiar <hianaguiar@hotmal.com>
2019-12-29 09:25:39 -0300

    f

---
+++ routers/items.py
@@ -0,0 +1,17 @@
+from faspapi import ApiRouter
+
+
+route=ApiRouter()
+
+@route.get('/')
+def get():
+    return     
+@route.post('/')
+def get():
+    return     
+@route.put('/')
+def get():
+    return     
+@route.delete('/')
+def get():
+    return     
